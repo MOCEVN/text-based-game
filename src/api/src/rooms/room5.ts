1ddf8606457e6b8b371fde65f60ce1073be866a8
Nii Sackey <sackeyn@ad.hva.nl>
2024-02-28 22:57:11 +0100

    kamer 5 foto

--- src/api/src/rooms/room5.ts
+++ src/api/src/rooms/room5.ts
@@ -1,13 +1,13 @@
 import { ActionResult } from "../base/actionResults/ActionResult";
 import { TextActionResult } from "../base/actionResults/TextActionResult";
+import { Action } from "../base/actions/Action";
+import { ExamineAction } from "../base/actions/ExamineAction";
+import { GameObject } from "../base/gameObjects/GameObject";
 import { Room } from "../base/gameObjects/Room";
-
 
 export const Room5Alias: string = "";
 
-
-export class Room5 extends Room{
-    
+export class Room5 extends Room {
     public constructor() {
         super(Room5Alias);
     }
@@ -16,5 +16,18 @@ export class Room5 extends Room{
     }
     public name(): string {
         return "Room5";
+    }
+    public actions(): Action[] {
+        return [new ExamineAction()];
+    }
+
+    public objects(): GameObject[] {
+        return [this];
+    }
+
+    public images(): string[] {
+        return [
+            "kamer5"
+        ];
     }
 }
