3222d520ed0377345ea5d82442aed63ae0477397
Megan <megan.sastroredjo@hva.nl>
2024-03-16 16:29:41 +0100

    Skeletons dialogen

--- src/api/src/characters/Skeleton Characters/Skeleton1.ts
+++ src/api/src/characters/Skeleton Characters/Skeleton1.ts
@@ -10,54 +10,69 @@ import { PlayerSession } from "../../types";
 
 export const SkeletonCharacter1Alias: string = "skeleton-character-1";
 
-export class SkeletonCharacter1 extends Character implements Examine { 
-  public constructor() { 
-    super(SkeletonCharacter1Alias, ExamineActionAlias);
-  }
-
-  public name(): string {
-    return "Skeleton 1";
-  }
-
-  public examine(): ActionResult | undefined {
-    return new TextActionResult(["You've met the first skeleton"]);
-  }
-
-  public talk(choiceId?: number | undefined): ActionResult | undefined {
-    const playerSession: PlayerSession = getPlayerSession();
-
-    if(choiceId === 1) {
-      return new TextActionResult([
-        "Skeleton: 'Hey there, pal! The name's Boney McRibcage.",
-        "Yep, I used to be a regular Joe until this ol' haunted house got the best of me.",
-        "Now I'm just hangin' around, quite literally. Heh, get it? Hangin' around?",
-        "Anyway, watch your step in this joint, friend. Things can get pretty spooky around here.",
-        "So, what's your deal? Looking for treasure? Trying to break the curse? Or just lost your way to the local sandwich shop?",]);
-    } else if(choiceId === 2) {
-      return new TextActionResult(["You need to investigate the room further."]);
+export class SkeletonCharacter1 extends Character implements Examine {
+    public constructor() {
+        super(SkeletonCharacter1Alias, ExamineActionAlias);
     }
-     else if(choiceId === 3) {
-      playerSession.inventory = [];
 
-      return new TextActionResult(["You've lost the code..."]);
-    } 
-
-    const choiceActions: TalkChoiceAction[] = [
-      new TalkChoiceAction(1, "Talk to the skeleton"),
-      new TalkChoiceAction(2, "Continue the quest"),
-    ];
-
-    if(playerSession.inventory.includes(ThreeNumberItemAlias)) {
-        choiceActions.push(new TalkChoiceAction(3, "Throw the code!"));
+    public name(): string {
+        return "Skeleton 1";
     }
-    
-    return new TalkActionResult(this, 
-      [ "As you approach the skeletal figure, you notice its weathered bones glistening faintly in the dim light of the room.",
-      "The skeleton is propped up against a crumbling pillar, its empty eye sockets seeming to watch your every move.",
-      "Despite its appearance, there's a playful aura about it, as if it's waiting for someone to engage in conversation.",
-      "Perhaps talking to the skeleton could reveal more about this haunted place and the secrets it holds."], 
-      choiceActions
-      );
-  }
 
+    public examine(): ActionResult | undefined {
+        return new TextActionResult(["You noticed a sleepy skeleton, it's holding a weathered book. With a clue maybe?"]);
+    }
+
+    public talk(choiceId?: number | undefined): ActionResult | undefined {
+        const playerSession: PlayerSession = getPlayerSession();
+
+        if (choiceId === 1) {
+            // Action for shaking the skeleton
+            const choiceActions: TalkChoiceAction[] = [
+                new TalkChoiceAction(4, " 'Is there any way you can help me out of here...' "),
+                new TalkChoiceAction(2, "Continue the quest"),
+            ];
+            if (playerSession.inventory.includes(ThreeNumberItemAlias)) {
+                choiceActions.push(new TalkChoiceAction(3, "Throw the code!"));
+            }
+            return new TalkActionResult(this,
+                ["*You gently shake the skeleton, hoping to wake it up*",
+                "Boney McRibage: 'Hey there, pal! The name's Boney McRibcage.",
+                "Yep, I used to be a regular Joe until this ol' haunted house got the best of me.",
+                "Now I'm just hangin' around, quite literally. Heh, get it? Hangin' around?",
+                "Anyway, watch your step friend. Things can get pretty spooky around here.",
+                "Oh, and about this book I'm holding, don't let it fool ya. It's just a bunch of gibberish, no help to anyone.",
+                "So, what's your deal? Trying to break the curse? Or just lost your way to the local sandwich shop?"
+
+                ],
+                choiceActions
+            );
+        } else if (choiceId === 2) {
+            // Action for continuing the quest
+            return new TextActionResult(["You decide to continue your exploration, perhaps there's more to uncover..."]);
+        } else if (choiceId === 3) {
+            // Action for throwing the code
+            playerSession.inventory = [];
+            return new TextActionResult(["You've lost the code..."]);
+        } else if (choiceId === 4) {
+            // Action for asking for help
+            return new TextActionResult([
+                "Boney McRibage: 'I'm sorry pal, I cannot help you with that... but maybe one of my friends can.' ",
+                " 'I don't remember which one again haha! Goodluck fella.' "
+            ]);
+        }
+
+        // Default action when no specific choice is selected
+        return new TalkActionResult(this,
+            ["As you cautiously approach the skeleton, you hear a soft snoring sound emanating from its gaping jaw, indicating that it's asleep.",
+                "You realize that you'll need to wake it up to proceed further."
+            ],
+            [
+                new TalkChoiceAction(1, " *shake the skeleton* "),
+                new TalkChoiceAction(2, "Continue the quest"),
+            ]
+        );
+    }
 }
+
+
--- src/api/src/characters/Skeleton Characters/Skeleton2.ts
+++ src/api/src/characters/Skeleton Characters/Skeleton2.ts
@@ -8,53 +8,58 @@ import { getPlayerSession } from "../../instances";
 import { ThreeNumberItemAlias } from "../../items/ThreeNumberItem";
 import { PlayerSession } from "../../types";
 
-
 export const SkeletonCharacter2Alias: string = "skeleton-character-2";
 
-export class SkeletonCharacter2 extends Character implements Examine { 
-  public constructor() { 
-    super(SkeletonCharacter2Alias, ExamineActionAlias);
-  }
-
-  public name(): string {
-    return "Skeleton 2";
-  }
-
-  public examine(): ActionResult | undefined {
-    return new TextActionResult(["You're walking slowly towards a skeleton, hoping you'll get closer to an answer."]);
-  }
-
-  public talk(choiceId?: number | undefined): ActionResult | undefined {
-    const playerSession: PlayerSession = getPlayerSession();
-
-    if(choiceId === 1) {
-      return new TextActionResult(["Skeleton 2: Lonely wanderer, your fate may intertwine with mine. Seek the clues to the realm beyond.", "My end came swiftly in the darkness, and now I linger in this eternal state.",
-      "To keep your pretty face you first have to solve the riddle."]);
-    } else if(choiceId === 2) {
-      return new TextActionResult(["You chickened out."]);
+export class SkeletonCharacter2 extends Character implements Examine {
+    public constructor() {
+        super(SkeletonCharacter2Alias, ExamineActionAlias);
     }
-    else if(choiceId === 3) {
-      playerSession.inventory = [];
 
-      return new TextActionResult(["You've lost the code..."]);
-    } 
-
-    const choiceActions: TalkChoiceAction[] = [
-      new TalkChoiceAction(1, "Talk to the skeleton"),
-      new TalkChoiceAction(2, "Runaway from the skeleton"),
-    ];
-
-    if(playerSession.inventory.includes(ThreeNumberItemAlias)) {
-        choiceActions.push(new TalkChoiceAction(3, "Throw the code!"));
+    public name(): string {
+        return "Skeleton 2";
     }
-    
-    return new TalkActionResult(this, 
-      [ "The skeleton clutching a faded parchment seems frozen in time, its skeletal hand forever grasping the aged document.",
-      "As you inspect the parchment, you decipher fragments of a forgotten tale – a tragic story of love and betrayal.",
-      "The faded ink tells of a bygone era, where kingdoms clashed, and an ill-fated romance unfolded amidst the chaos.",
-      "Though the details are obscured, the parchment hints at a hidden truth waiting to be unraveled."], 
-      choiceActions
-      );
 
-  }
+    public examine(): ActionResult | undefined {
+        return new TextActionResult(["You're walking slowly towards a skeleton, hoping you'll get closer to an answer."]);
+    }
+
+    public talk(choiceId?: number | undefined): ActionResult | undefined {
+        const playerSession: PlayerSession = getPlayerSession();
+
+        if (choiceId === 1) {
+            let choiceActions: TalkChoiceAction[] = [
+                new TalkChoiceAction(1, "Read the book"),
+                new TalkChoiceAction(2, "Continue to read the book"),
+                new TalkChoiceAction(3, "Run for your life"),
+            ];
+
+            if (playerSession.inventory.includes(ThreeNumberItemAlias)) {
+                choiceActions.push(new TalkChoiceAction(3, "Throw the code!"));
+            }
+
+            return new TalkActionResult(this, ["The skeleton clutching a book seems frozen in time.",
+            "As you inspect the book, you decipher fragments of a forgotten tale, a tragic story of love and betrayal.",
+            "The faded ink tells..."], 
+            choiceActions);
+        } else if (choiceId === 2) {
+            return new TextActionResult(["Ethel Baker: 'Pathetic mortal, this is not for your eyes! You think you can outwit the darkness that dwells here? Foolishness! Yet, perhaps there's a glimmer of hope for you, hidden within these cursed pages. The item you seek lies within these ancient words, but only the worthy may unlock its secrets. Proceed if you dare, but remember, not all who seek knowledge survive the journey.' "]);
+        } else if (choiceId === 3) {
+            return new TextActionResult(["You chickened out."]);
+        }
+
+        // Default action when no specific choice is selected
+        return new TalkActionResult(this,
+            ["You see that the skeleton seems to be holding a book. Perhaps talking to it could provide some insight?"
+            ],
+            [
+                new TalkChoiceAction(1, "Read the book"),
+                new TalkChoiceAction(3, "Run for your life"),
+            ]
+        );
+    }
 }
+
+
+
+
+
